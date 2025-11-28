9da0a30289b4942a45ceb6c90bf6243641b8faf0
Vensontzy <venznagutz0814@gmail.com>
2025-11-28 12:25:04 +0000

    Add

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const http = require("http");
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.write("Hello Cloud!!!");
-  res.write("Welcome to my page.");
+  res.write("Nagutom Venson C. \n");
+  res.write("ITBA-4101 \n");
+  res.write("Attitude > Inteligence");
   res.end();
 });
 
